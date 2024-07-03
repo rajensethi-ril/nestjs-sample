7f6a3d6b30bb74f6054d1a8550b339f7110ea821
Pratiksha Shende <pratiksha.shende@ril.com>
2024-07-03 13:16:47 +0000

    ci:base code

--- src/client-branding/client-branding.module.ts
+++ src/client-branding/client-branding.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ClientBrandingAggregateService } from './aggregates/aggregate.service';
+import { ClientBrandingCommandService } from './commands/command.service';
+import { ClientBrandingControllerService } from './controllers/controller.service';
+import { ClientBrandingEntitiesService } from './entities/entities.service';
+import { ClientBrandingEventsService } from './events/events.service';
+import { ClientBrandingPoliciesService } from './policies/policies.service';
+import { ClientBrandingQueriesService } from './queries/queries.service';
 
 @Module({})
-export class ClientBrandingModule {}
+export class ClientBrandingModule {
+  imports: [
+    ClientBrandingAggregateService,
+    ClientBrandingCommandService,
+    ClientBrandingControllerService,
+    ClientBrandingEntitiesService,
+    ClientBrandingEventsService,
+    ClientBrandingPoliciesService,
+    ClientBrandingQueriesService,
+  ];
+}
